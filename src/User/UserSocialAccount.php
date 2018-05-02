2db71a3f7f399cffacfa9f626db8721fe5bb5b23
Anand Siddharth <anandsiddharth@bitbucket.org>
2018-05-02 22:45:24 +0530

    user social account interfce

---
+++ src/User/UserSocialAccount.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User social account interface.
+ *
+ * @author      Anand Siddharth <anandsiddharth21@gmail.com>
+ * @copyright   Copyright (c) Anand Siddharth
+ * @license     http://mit-license.org/
+ *
+ * @link        https://github.com/anandsiddharth/laravel-passport-socialite
+ */
+namespace Anand\Laravel\PassportSocialite\User;
+
+interface UserSocialAccount {
+    /**
+     * Get user from social provider from provider's user's id
+     * 
+     * @param string $provider Provider name as requested from oauth e.g. facebook
+     * @param string $id Id used by provider
+     */
+    public function getUserFromProvider($provider, $id); 
+}
