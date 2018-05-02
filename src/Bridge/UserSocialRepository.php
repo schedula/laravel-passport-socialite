1de4bd923de862e6570a1683f6e8aa35d59b5e32
Anand Siddharth <anandsiddharth@bitbucket.org>
2018-05-02 23:35:17 +0530

    return user if found

--- src/Bridge/UserSocialRepository.php
+++ src/Bridge/UserSocialRepository.php
@@ -5,6 +5,7 @@ namespace Anand\Laravel\PassportSocialite\Bridge;
 
 use Anand\League\OAuth2\Server\Repositories\UserSocialRepositoryInterface;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
+use  Laravel\Passport\Bridge\User;
 use Socialite;
 use InvalidArgumentException;
 
@@ -30,8 +31,21 @@ class UserSocialRepository implements UserSocialRepositoryInterface {
     public function getUserFromSocialProvider($accessToken, $provider, $grantType, ClientEntityInterface $clientEntity){
         try {
             $socialite = Socialite::with($provider);
-            $user = $socialite->userFromToken($accessToken);
-            
+            $socialUser = $socialite->userFromToken($accessToken);
+
+            $provider = config('auth.guards.api.provider');
+            if (is_null($model = config('auth.providers.'.$provider.'.model'))) {
+                throw OAuthServerException::serverError('Unable to determine authentication model from configuration.');
+            }
+            if (method_exists($model, 'findForPassportSocialite')) {
+                $user = $model::findForPassportSocialite($provider, $socialUser->getId());
+                if(!$user) {
+                    return;
+                }
+                return new User($user->getAuthIdentifier());
+            }else{
+                throw OAuthServerException::serverError('method "findForPassportSocialite" not implemented in authentication model from configuration.');                
+            }
         } catch (InvalidArgumentException $e) {
             throw OAuthServerException::invalidRequest('provider');
         } catch (\Exception $e) {
